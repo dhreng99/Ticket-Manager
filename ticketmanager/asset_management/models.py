d173c3a0baf0e6e4bdacabacc92482c44a65251e
dhreng99 <dominic.eng@me.com>
2023-08-25 13:44:56 +0100

    permissions

--- ticketmanager/asset_management/models.py
+++ ticketmanager/asset_management/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import Permission
 
 class AssetCategory(models.Model):
     name = models.CharField(max_length=100)
@@ -6,7 +9,14 @@ class AssetCategory(models.Model):
 
     def __str__(self):
         return self.name
-
+    
+class Meta:
+        permissions = [
+            ("can_view_assetcategory", "Can view asset category"),
+            ("can_add_assetcategory", "Can add asset category"),
+            ("can_change_assetcategory", "Can change asset category"),
+            ("can_delete_assetcategory", "Can delete asset category"),
+        ]
 class Asset(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
@@ -15,3 +25,11 @@ class Asset(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        permissions = [
+            ("can_view_asset", "Can view asset"),
+            ("can_add_asset", "Can add asset"),
+            ("can_change_asset", "Can change asset"),
+            ("can_delete_asset", "Can delete asset"),
+        ]
