079bacd29dc7b807cbcbd5999da2cbfd9af1af20
dhreng99 <dominic.eng@me.com>
2023-08-25 11:29:54 +0100

    register and login

--- ticketmanager/asset_management/views.py
+++ ticketmanager/asset_management/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('home')  # Replace 'home' with the name of the view to redirect after login.
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
